023d224962fb9f6ced34cdf559250e1e113dfa79
project-srs <project@srs>
2025-03-15 05:14:28 +0900

    update

--- srs_simple_tablet_server_nodes/srs_simple_tablet_server_nodes/api_server.py
+++ srs_simple_tablet_server_nodes/srs_simple_tablet_server_nodes/api_server.py
@@ -14,84 +14,31 @@ function
 * note (service: ~/set_note - websocket /status note)
 '''
 
-import sys
-import fastapi
-import uvicorn
-import pydantic
-import uuid
 import os
-from starlette.middleware.cors import CORSMiddleware
 import time
 import threading
 import json
+import sys
+
+# FastAPI
+import fastapi
+import uvicorn
+import pydantic
+from starlette.middleware.cors import CORSMiddleware
 import asyncio
 
 # ROS2
 import rclpy
 from rclpy.node import Node
 from rclpy.executors import MultiThreadedExecutor
-from std_msgs.msg import String
 from rclpy.action import ActionServer, CancelResponse
-
-
 from srs_simple_tablet_server_msgs.action  import SelectTask
 from srs_simple_tablet_server_msgs.srv import SetNote
 
 ####### Data Type #######
-
-class NoteItem:
-    def __init__(self, level, message):
-        self.level = level
-        self.message = message
-
-class OptionItem:
-    def __init__(self, name, endpoint, post_content):
-        self.name = name
-        self.endpoint = endpoint
-        self.post_content = post_content
-
-class ModePost(pydantic.BaseModel):
-    name: str
-    comment: str
-
-class TagPost(pydantic.BaseModel):
-    category: str
-    serial: int
-
-class ActionPost(pydantic.BaseModel):
-    name: str
-    tag: TagPost
-
-class VehicleStatus:
-    def __init__(self):
-        self.mode_name ='unknown'
-        self.mode_color = 'gray'
-        self.board_icon = "stop"
-        self.board_message = "stop"
-
-        self.action_list = []
-        self.mode_list = []
-        self.note_dict = {}
-
-    def get_json_string(self):
-        data_dic = {
-            "mode_name":self.mode_name,
-            "mode_color":self.mode_color,
-            "board_icon":self.board_icon,
-            "board_message":self.board_message,
-            "modes": [],
-            "actions":[],
-            "notes":[],
-            "modes":[]
-        }
-        for mode_item in self.mode_list:
-            data_dic["modes"].append(vars(mode_item))
-        for action_item in self.action_list:
-            data_dic["actions"].append(vars(action_item))
-        for note_value in self.note_dict.values():
-            data_dic["notes"].append(vars(NoteItem(note_value[1], note_value[2])))
-
-        return json.dumps(data_dic)
+class SelectPostData(pydantic.BaseModel):
+    goal_id: str
+    key: str
 
 app = fastapi.FastAPI(debug=True)
 app.add_middleware(
@@ -105,149 +52,182 @@ app.add_middleware(
 class Ros2ApiServer(Node):
     def __init__(self):
         super().__init__('api_server')
-        self.declare_parameter('mode_name', 'manual')
-        self.declare_parameter('mode_color', 'gray')
-        self.declare_parameter('mode_list', ["HOLD", "MANUAL", "ROUND", 'GOTO', 'Transport', "FOLLOW"])
-        self.declare_parameter('board_icon', 'pause')
-        self.declare_parameter('board_message', 'gray')
 
-        self.last_vehicle_status = VehicleStatus()
-        # self.last_vehicle_status.note_list.append(NoteItem("info", "this is normal message."))
-        # self.last_vehicle_status.note_list.append(NoteItem("warning", "this is warning message."))
-        # self.last_vehicle_status.note_list.append(NoteItem("error", "this is error message."))
+        # drawer_select
+        self.drawer_select_action_server = ActionServer(self, SelectTask, '~/select/drawer', self.drawer_select_callback, cancel_callback=self.cancel_accept_callback)
+        self.drawer_select_goal_handle_dic = {}
+        self.drawer_select_post_data_dic = {}
 
-        # ROS2 API
-        self.mode_pub = self.create_publisher(String, "~/mode_event", 1)
-        self.note_srv = self.create_service(SetNote, "~/set_note", self._set_note_callback)
-        self.interval_timer = self.create_timer(0.5, self._timerCallback)
-        self._action_server = ActionServer(
-            self,
-            SelectTask,
-            '~/select_task',
-            self._selectExecuteCallback,
-            cancel_callback=self.cancel_callback)
-        self._action_result = None
-        self._action_forcus_id = None
+        # action_select
+        self.action_select_action_server = ActionServer(self, SelectTask, '~/select/action', self.action_select_callback, cancel_callback=self.cancel_accept_callback)
+        self.action_select_goal_handle_dic = {}
+        self.action_select_post_data_dic = {}
 
-        ####### Mode #######
-        @app.get("/mode")
-        async def get_mode():
-            return self.last_vehicle_status.mode_name
+        # display
+        self.declare_parameter('display.header.name', 'manual')
+        self.declare_parameter('display.header.color', 'gray')
+        self.declare_parameter('display.board.icon', 'pause')
+        self.declare_parameter('display.board.message', 'none')
 
-        @app.post("/mode")
-        async def post_mode(mode: ModePost):
-            self.get_logger().info("post_mode: " + str(mode))
-            self.publishMode(mode.name)
-            return mode
+        # note
+        self.note_srv = self.create_service(SetNote, "~/set_note", self.set_note_callback)
+        self.note_data_dic = {}
+        self.interval_timer = self.create_timer(0.5, self.timer_callback)
 
-        ####### action #######
-        @app.post("/action")
-        async def post_action(action: ActionPost):
-            self.get_logger().info('/action ' + str(action))
-            self._action_result = action
-            return action
+        @app.post("/select/drawer")
+        async def post_drawer_select(post_data: SelectPostData):
+            self.get_logger().info("post_drawer_select: " + str(post_data))
+            self.drawer_select_post_data_dic[post_data.goal_id] = post_data
+            return fastapi.responses.JSONResponse(status_code=fastapi.status.HTTP_200_OK)
 
-        ####### status #######
+        @app.post("/select/action")
+        async def post_action_select(post_data: SelectPostData):
+            self.get_logger().info("post_action_select: " + str(post_data))
+            self.action_select_post_data_dic[post_data.goal_id] = post_data
+            return fastapi.responses.JSONResponse(status_code=fastapi.status.HTTP_200_OK)
+
         @app.websocket("/status")
         async def websocket_endpoint(websocket: fastapi.WebSocket):
             try:
                 await websocket.accept()
                 self.get_logger().info("connetct: " + websocket.headers.get("sec-websocket-key"))
                 while True:
-                    self.last_vehicle_status.mode_name = self.get_parameter('mode_name').get_parameter_value().string_value
-                    self.last_vehicle_status.mode_color = self.get_parameter('mode_color').get_parameter_value().string_value
-                    self.last_vehicle_status.board_icon = self.get_parameter('board_icon').get_parameter_value().string_value
-                    self.last_vehicle_status.board_message = self.get_parameter('mode_color').get_parameter_value().string_value
-                    self.last_vehicle_status.mode_list = []
-                    for mode in self.get_parameter('mode_list').get_parameter_value().string_array_value:
-                        self.last_vehicle_status.mode_list.append(OptionItem(mode, "/mode", json.dumps({"name":mode, "comment":""})))
-                    await websocket.send_text(self.last_vehicle_status.get_json_string())
+                    await websocket.send_text(self.generate_status())
                     await asyncio.sleep(0.5)
             except Exception as e:
                 self.get_logger().info("disconnected: " + str(e))
 
-    def publishMode(self, mode):
-        msg = String()
-        msg.data = mode
-        self.mode_pub.publish(msg)
+    def drawer_select_callback(self, goal_handle):
+        goal_id_str = str(bytes(goal_handle.goal_id.uuid).hex())
+        self.get_logger().info(f'drawer_select_callback: {goal_id_str}')
+        self.drawer_select_goal_handle_dic[goal_id_str] = goal_handle.request
 
-    def _timerCallback(self):
-        ros_now = self.get_clock().now()
-        delet_keys = [key for key, value in self.last_vehicle_status.note_dict.items() if 5.0 * (10 ** 9) < (ros_now - value[0]).nanoseconds]
-        for key in delet_keys:
-            del self.last_vehicle_status.note_dict[key]
+        result = SelectTask.Result()
+        while goal_handle.is_active:
+            if goal_handle.is_cancel_requested:
+                # canceled
+                self.get_logger().info('drawer_select: cancel request')
+                goal_handle.canceled()
+                break
 
-    def _set_note_callback(self, request, response):
-        # print(request)
+            elif goal_id_str in self.drawer_select_post_data_dic.keys():     
+                # receive post data
+                self.get_logger().info('drawer_select: get post data')
+                post_data = self.drawer_select_post_data_dic[goal_id_str]
+                goal_handle.succeed()
+
+                result.selected_key = post_data.key
+                del self.drawer_select_post_data_dic[goal_id_str]
+                break
+
+            feedback_msg = SelectTask.Feedback()
+            goal_handle.publish_feedback(feedback_msg)
+            time.sleep(0.5)
+        else:
+            self.get_logger().info('drawer_select: error')
+            goal_handle.abort()
+
+        del self.drawer_select_goal_handle_dic[goal_id_str]
+        return result
+
+    def action_select_callback(self, goal_handle):
+        goal_id_str = str(bytes(goal_handle.goal_id.uuid).hex())
+        self.get_logger().info(f'action_select_callback: {goal_id_str}')
+        self.action_select_goal_handle_dic[goal_id_str] = goal_handle.request
+
+        result = SelectTask.Result()
+        while goal_handle.is_active:
+            if goal_handle.is_cancel_requested:
+                # canceled
+                self.get_logger().info('action_select: cancel request')
+                goal_handle.canceled()
+                break
+
+            elif goal_id_str in self.action_select_post_data_dic.keys():     
+                # receive post data
+                self.get_logger().info('action_select: get post data')
+                post_data = self.action_select_post_data_dic[goal_id_str]
+                goal_handle.succeed()
+
+                result.selected_key = post_data.key
+                del self.action_select_post_data_dic[goal_id_str]
+                break
+
+            feedback_msg = SelectTask.Feedback()
+            goal_handle.publish_feedback(feedback_msg)
+            time.sleep(0.5)
+        else:
+            self.get_logger().info('action_select: error')
+            goal_handle.abort()
+
+        del self.action_select_goal_handle_dic[goal_id_str]
+        return result
+
+    def cancel_accept_callback(self, goal_handle):
+        goal_id_str = str(bytes(goal_handle.goal_id.uuid).hex())
+        self.get_logger().info(f'cancel_accept_callback: {goal_id_str}')
+        return CancelResponse.ACCEPT
+
+    def set_note_callback(self, request, response):
         ros_now = self.get_clock().now()
-        tag_str = request.tag.category + "/" + str(request.tag.serial)
-        level_str = "info"
-        if request.level == SetNote.Request.LEVEL_WARNING:
-            level_str = "warning"
-        elif request.level == SetNote.Request.LEVEL_ERROR:
-            level_str = "error"
-        self.last_vehicle_status.note_dict[tag_str] = [ros_now, level_str, request.content]
+        self.note_data_dic[request.key] = [ros_now, request]
         response.success = True
         return response
 
-    def _selectExecuteCallback(self, goal_handle):
-        self.get_logger().info('Executing goal...')
+    def timer_callback(self):
+        ros_now = self.get_clock().now()
+        delete_duration = 5.0
+        delet_keys = [key for key, value in self.note_data_dic.items() if delete_duration * (10 ** 9) < (ros_now - value[0]).nanoseconds]
+        for key in delet_keys:
+            del self.note_data_dic[key]
 
-        self.last_vehicle_status.action_list = []
-        for option in goal_handle.request.option_list:
-            content = {
-                "name": option.name,
-                "tag" : {
-                    "category" : option.tag.category,
-                    "serial" : option.tag.serial
-                }
-            }
-            self.last_vehicle_status.action_list.append(
-                OptionItem(option.name, "/action", json.dumps(content))
-            )
-        self._action_result = None
-        self._action_forcus_id = goal_handle.goal_id
+    def generate_status(self):
+        def generate_options(goal_handle_dic):
+            output_list = []
+            for goal_id, request in goal_handle_dic.items():
+                for option in request.option_list:
+                    output_list.append({"goal_id":goal_id, "key":option.key, "display_name":option.display_name})
+            return output_list
 
-        feedback_msg = SelectTask.Feedback()
-        while goal_handle.is_active and goal_handle.goal_id == self._action_forcus_id:
-            if goal_handle.is_cancel_requested:
-                # canceled
-                self.get_logger().info('cancel goal...')
-                result = SelectTask.Result()
-                self._action_result = None
-                self.last_vehicle_status.action_list = []
-                goal_handle.canceled()
-                print("not active")
-                return result
-            elif self._action_result is not None:     
-                # goal
-                result = SelectTask.Result()
-                result.target_tag.category = self._action_result.tag.category
-                result.target_tag.serial = self._action_result.tag.serial
-                self._action_result = None
-                self.last_vehicle_status.action_list = []
-                goal_handle.succeed()
-                print("goal")
-                return result
+        # drawer_select
+        drawer_select_data = {}
+        drawer_select_data["options"] = generate_options(self.drawer_select_goal_handle_dic)
 
-            # print("sleep", goal_handle.goal_id)
-            time.sleep(0.5)
+        # action_select
+        action_select_data = {}
+        action_select_data["options"] = generate_options(self.action_select_goal_handle_dic)
 
-        goal_handle.abort()
-        result = SelectTask.Result()
-        self.get_logger().error('focus to another goal')
-        return result
+        # display
+        display_data = {}
+        display_data["header"] = {}
+        display_data["header"]["name"] = self.get_parameter('display.header.name').get_parameter_value().string_value
+        display_data["header"]["color"] = self.get_parameter('display.header.color').get_parameter_value().string_value
+        display_data["board"] = {}
+        display_data["board"]["icon"] = self.get_parameter('display.board.icon').get_parameter_value().string_value
+        display_data["board"]["message"] = self.get_parameter('display.board.message').get_parameter_value().string_value
 
-    def cancel_callback(self, goal):
-        """Accepts or rejects a client request to cancel an action."""
-        print("cancel_callback")
-        return CancelResponse.ACCEPT
+        # note
+        note_data = {}
+        note_data["contents"] = []
+        for item in self.note_data_dic.values():
+            level = "none"
+            if item[1].level == SetNote.Request.LEVEL_INFO:
+                level = "info"
+            elif item[1].level == SetNote.Request.LEVEL_WARNING:
+                level_str = "warning"
+            elif item[1].level == SetNote.Request.LEVEL_ERROR:
+                level_str = "error"
+            note_data["contents"].append({"level":level, "message":item[1].message})
 
-####### main ######
+        # output
+        send_data = {}
+        send_data["drawer_select"] = drawer_select_data
+        send_data["action_select"] = action_select_data
+        send_data["display"] = display_data
+        send_data["note"] = note_data
+        return json.dumps(send_data)
+
 def main(argv=sys.argv):
-    # uvi_thread = threading.Thread(target=uvicorn.run, args=(app,), kwargs={"host":"", "port":8010, "log_level":"warning"}, daemon=True)
-    # uvi_thread.start()
-
     rclpy.init()
     node = Ros2ApiServer()
 
